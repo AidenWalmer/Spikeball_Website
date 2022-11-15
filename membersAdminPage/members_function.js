3f5fc96e501f333dd68b2f1543de118dcfc27396
AidenWalmer <aidenjwalmer@gmail.com>
2022-11-15 17:56:44 -0500

    deleted extra files

--- membersAdminPage/members_function.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', init);    // when window is loaded, it will call init
-
-function init() {
-    let adminBtns = document.getElementById("adminBtns");
-    adminBtns.classList.remove("hidden");
-}
