8b0490cda653c3cbf5ccf066234a60d7efa203da
AidenWalmer <aidenjwalmer@gmail.com>
2022-11-15 16:05:00 -0500

    temporarily unhide admin buttons

--- membersPage/members_function.js
+++ membersPage/members_function.js
@@ -1,13 +1,15 @@
 window.addEventListener('load', init);    // when window is loaded, it will call init
 
 function init() {
-
-    if (loggedIn) {
-        console.log("Admin Session: " + loggedIn);
-        let adminBtns = document.getElementById("adminBtns");
-        adminBtns.classList.remove("hidden");
-    }
-    else {
-        console.log("Admin Session: " + loggedIn);
-    }
+    let adminBtns = document.getElementById("adminBtns");
+    adminBtns.classList.remove("hidden");
+    
+    // if (loggedIn) {
+    //     console.log("Admin Session: " + loggedIn);
+    //     let adminBtns = document.getElementById("adminBtns");
+    //     adminBtns.classList.remove("hidden");
+    // }
+    // else {
+    //     console.log("Admin Session: " + loggedIn);
+    // }
 }
