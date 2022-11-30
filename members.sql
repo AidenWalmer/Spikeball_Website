63a78480754a620c8ebb0aabca9fe7b99e72377a
AidenWalmer <aidenjwalmer@gmail.com>
2022-11-30 11:54:53 -0500

    fixed SQL files

--- members.sql
+++ members.sql
@@ -1,14 +1,3 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema spikeball
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Table `members`
--- -----------------------------------------------------
 USE spikeballdb;
 DROP TABLE IF EXISTS members;
 CREATE TABLE IF NOT EXISTS members (
@@ -57,9 +46,3 @@ INSERT INTO members(nameFirst, nameLast, startDate, gradYear, email) VALUES ('Ja
 INSERT INTO members(nameFirst, nameLast, startDate, gradYear, email) VALUES ('Evan', 'Carneal', '2022-09-01', '0', 'carneale@etown.edu');
 INSERT INTO members(nameFirst, nameLast, startDate, gradYear, email) VALUES ('Hannah', 'Lewiski', '2022-11-11', '2025', 'lewiskih@etown.edu');
 INSERT INTO members(nameFirst, nameLast, startDate, gradYear, email) VALUES ('Christain', 'Ramos', '2022-11-11', '2026', 'ramosc@etown.edu');
-
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- users.sql
+++ users.sql
@@ -1,8 +1,18 @@
-use users
-SELECT *
-FROM users
+USE spikeballdb;
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DELETE FROM users WHERE userid >= 1;
+INSERT INTO `users` (`userid`, `username`, `password`) VALUES
+(1, 'spikeballclub', '$2y$10$Kgpka2dN29Z.nS7QZrSaXuGjLhWd6dAmEtsjfrdtzd89QW8.3paCG');
 
-ALTER TABLE users auto_increment = 1;
-UPDATE users SET userid = 1;
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `userid_UNIQUE` (`userid`);
+
+ALTER TABLE `users`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
